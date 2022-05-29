88ff6fccf83ae335318ef843cee09b0cacea9cdb
TomBohbot <bohbot.tom@gmail.com>
2022-05-29 15:39:57 -0400

    Render endpoints async

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from util import clean_dna, valid_dna
+import asyncio
 import redis
 
 
@@ -8,14 +9,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+async def index():
     ip_address = request.remote_addr    
-    data = r.lrange(f"{ip_address}::prev_query", 0, -1 )
+    data = r.lrange(f"{ip_address}::prev_query", 0, -1 )  
     return render_template('index.html', data=data)
 
 
 @app.route('/',methods=['POST'])
-def dna_request():
+async def dna_request():
     ip_address = request.remote_addr
     dna = clean_dna(request.form["dna_seq"])
     print(f"Inputted DNA Sequence: {dna}")    
