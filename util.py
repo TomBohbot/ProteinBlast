bd0ea050d780c20fc2545388692c97b45e7ea4cb
TomBohbot <bohbot.tom@gmail.com>
2022-05-29 15:04:02 -0400

    Refactored Files

--- util.py
+++
@@ -1,7 +0,0 @@
-def clean_dna(dna: str) -> str:
-    return dna.upper().strip()
-
-def valid_dna(dna: str) -> bool:
-    if set(dna).union(set("ACTG") ) != set("ACTG"):
-        return False
-    return True
