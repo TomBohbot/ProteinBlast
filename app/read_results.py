28613d2baa1ddcc4d4132cdd4200ca209efc66b2
TomBohbot <bohbot.tom@gmail.com>
2022-05-31 18:06:00 -0400

    Switch Protein and Genome

--- app/read_results.py
+++ app/read_results.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from constants import BLAST_HITS, BLAST_HIT_TYPES,    \
     BLAST_HIT_SEQ, OUTPUT_FILE_PATH, BLAST_ERROR_KEY
 
-def no_results(xml: str) -> bool:
+def no_results(xml: BeautifulSoup) -> bool:
     hit = xml.find(BLAST_HITS)
     error_message = xml.find(BLAST_ERROR_KEY)
     if len(hit.get_text()) == 1 and error_message.get_text() != None:
@@ -11,14 +11,14 @@ def no_results(xml: str) -> bool:
     return False
 
 
-def read_blast_results(dna: str) -> dict:
-    soup = BeautifulSoup(open(OUTPUT_FILE_PATH,'r'), 'xml')
+def read_blast_results(dna: str, file_path: str = OUTPUT_FILE_PATH) -> BlastResult:
+    soup = BeautifulSoup(open(file_path,'r'), 'xml')
     if no_results(soup):
         return None  
     hit = soup.find(BLAST_HITS)     
     return BlastResult(
         dna=dna,
-        genome=hit.find(BLAST_HIT_TYPES).get_text().split()[0], 
-        protein=hit.find(BLAST_HIT_TYPES).get_text().split()[1],
+        genome=hit.find(BLAST_HIT_TYPES).get_text().split()[1], 
+        protein=hit.find(BLAST_HIT_TYPES).get_text().split()[0],
         match=hit.find(BLAST_HIT_SEQ).get_text() 
     )
