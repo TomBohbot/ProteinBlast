d9a804523c9e5e682763c56adb2ee827102d3340
TomBohbot <bohbot.tom@gmail.com>
2022-05-31 00:17:01 -0400

    Add BLAST connection

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, jsonify, render_template
-from util import clean_sequence, valid_dna
-from blast import blast
-import asyncio
 import redis
-
+from blast import blast
+from verify_sequences import clean_sequence, valid_dna
+from flask import Flask, request, jsonify, render_template
+from constants import KEY, OUTPUT_SUCCESS, OUTPUT_ERROR, OUTPUT_EMPTY
 
 r = redis.StrictRedis('localhost', 6379, charset="utf-8", decode_responses=True)
 app = Flask(__name__)
@@ -12,20 +11,27 @@ app = Flask(__name__)
 @app.route('/')
 async def index():
     ip_address = request.remote_addr    
-    data = r.lrange(f"{ip_address}::prev_query", 0, -1 )  
+    data = r.lrange(f"{ip_address}::{KEY}", 0, -1 )  
     return render_template('index.html', data=data)
 
 
 @app.route('/',methods=['POST'])
 async def dna_request():
     ip_address = request.remote_addr
-    dna = clean_sequence(request.form["dna_seq"])
-    print(f"Inputted DNA Sequence: {dna}")    
+    dna = clean_sequence(request.form['dna_seq'])    
     if not valid_dna(dna):   
-        return jsonify("Improper DNA Sequence Inputted.")
-    # sequenced_dna = await blast(dna)
-    r.rpush(f"{ip_address}::prev_query", dna)    
-    return jsonify(f"The DNA you entered is {dna}.")
+        return jsonify(OUTPUT_ERROR)
+    sequenced_dna = await blast(dna) 
+    if sequenced_dna == None:
+        return OUTPUT_EMPTY
+    response = OUTPUT_SUCCESS.format(
+        sequenced_dna.dna, 
+        sequenced_dna.genome,
+        sequenced_dna.protein,
+        sequenced_dna.match
+    )
+    r.rpush(f"{ip_address}::{KEY}", response)
+    return response
 
 
 if __name__ == '__main__':
