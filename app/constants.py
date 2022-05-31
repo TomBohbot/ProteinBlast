1a6e0bb4a0e8a86ef02f7668de71780b11f07dc4
TomBohbot <bohbot.tom@gmail.com>
2022-05-31 00:18:03 -0400

    Add constants file to render project maintainable

---
+++ app/constants.py
@@ -0,0 +1,24 @@
+VALID_NUCLEOTIDE_SEQ = 'ACTG'
+VALID_PROTEIN_SEQ = 'ACDEFGHIKLMNPQRSTVWY'
+INPUT_ID = 'input_id'
+INPUT_NAME = 'input_name'
+INPUT_DESC = 'input dna to be sequenced'
+INPUT_FILE_PATH = 'input.fasta'
+OUTPUT_ERROR = 'Improper DNA Sequence Inputted.'
+KEY = 'prev_queries'
+DB_PATH = 'app/data/database/database.fasta'
+E_VALUE = 0.9
+OUTPUT_FILE_PATH = 'output.xml'
+OUTFMT = 5
+BLAST_HITS = 'Iteration_hits'
+BLAST_HIT_TYPES = 'Hit_def'
+BLAST_HIT_SEQ = 'Hsp_qseq'
+BLAST_ERROR_KEY = 'Iteration_message'
+BLAST_ERROR_MSG = 'No hits found'
+OUTPUT_EMPTY = 'No Matches Found.'
+OUTPUT_SUCCESS = '<br/>         \
+    Input DNA: {} <br/>         \
+    Matching Genome: {} <br/>   \
+    Matching Protein: {} <br/>  \
+    Matching Sequence: {} <br/> \
+    <br/>'
