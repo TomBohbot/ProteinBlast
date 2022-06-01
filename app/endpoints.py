8fb8e3852505484a37e159600aad1158b1aaa53f
TomBohbot <bohbot.tom@gmail.com>
2022-06-01 09:09:34 -0400

    basic logging

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,4 +1,5 @@
 import redis
+import logging
 from blast import blast
 from verify_sequences import clean_sequence, valid_dna
 from flask import Flask, request, jsonify, render_template
@@ -10,7 +11,8 @@ app = Flask(__name__)
 
 @app.route('/')
 async def index():
-    ip_address = request.remote_addr    
+    ip_address = request.remote_addr
+    logging.info(f'{ip_address} has visited site home page')
     data = r.lrange(f"{ip_address}::{KEY}", 0, -1 )  
     return render_template('index.html', data=data)
 
@@ -18,11 +20,15 @@ async def index():
 @app.route('/',methods=['POST'])
 async def dna_request():
     ip_address = request.remote_addr
-    dna = clean_sequence(request.form['dna_seq'])    
-    if not valid_dna(dna):   
+    input_dna = request.form['dna_seq']
+    logging.info(f'Received DNA input: {input_dna} from user: {ip_address}')
+    dna = clean_sequence(input_dna)    
+    if not valid_dna(dna):
+        logging.info(f'Sending user: {ip_address} inputted invalid DNA: {input_dna}')
         return jsonify(OUTPUT_ERROR)
     sequenced_dna = await blast(dna) 
     if sequenced_dna == None:
+        logging.info(f'Sending user: {ip_address} had no match on DNA: {dna}')
         return OUTPUT_EMPTY
     response = OUTPUT_SUCCESS.format(
         sequenced_dna.dna, 
@@ -31,8 +37,9 @@ async def dna_request():
         sequenced_dna.match
     )
     r.lpush(f"{ip_address}::{KEY}", response)
+    logging.info(f'Sending user: {ip_address} had a match on DNA: {response}')
     return response
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(debug=True)
